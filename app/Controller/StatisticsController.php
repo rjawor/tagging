ded3b01ec532fc9b27ccae970bbc844a0d951417
rjawor <rjawor@amu.edu.pl>
2016-10-26 14:23:12 +0200

    pagination on single words

--- app/Controller/StatisticsController.php
+++ app/Controller/StatisticsController.php
@@ -11,8 +11,9 @@ App::uses('WordAnnotationTypeChoice', 'Model');
 App::uses('WordAnnotationType', 'Model');
 
 
-
 class StatisticsController extends AppController {
+    private $RESULTS_PER_PAGE = 15;
+
     public function taggerdownload() {
         $this->viewClass = 'Media';
         // Download app/tmp/example.txt
@@ -23,7 +24,7 @@ class StatisticsController extends AppController {
             'extension' => 'txt',
             'path'      => APP . 'tmp' . DS
         );
-        
+
         DataDumper::dumpTagsToFile($params['path'].$params['id']);
         $this->set($params);
     }
@@ -41,18 +42,18 @@ class StatisticsController extends AppController {
             $this->set('languages', $languages);
 
             $this->set('documentFilter', array_key_exists('documentFilter', $this->request['data'])? $this->request['data']['documentFilter'] : 0  );
-            
+
             if (isset($this->request['data']['documentIds'])) {
     	        $documentIds = $this->request['data']['documentIds'];
             } else {
                 $documentIds = array();
             }
-            
+
             $this->set('documentIds', $documentIds);
-            
-            
-        
-        
+
+
+
+
             $sentenceModel = ClassRegistry::init('Sentence');
             $sentenceModel->recursive = 1;
             $query = "select *, sentences.id as sent_id, (select group_concat(case words.split when 1 then concat(words.stem, '|', words.suffix) else words.text end order by words.position separator ' ') from words where words.sentence_id = sent_id) as sentence_text from sentences inner join sentence_annotations on sentences.id = sentence_annotations.sentence_id and type_id = 1 and sentence_annotations.text != '' inner join documents on documents.id = sentences.document_id inner join languages on languages.id = documents.language_id";
@@ -78,47 +79,64 @@ class StatisticsController extends AppController {
                 $this->set('languages', $languages);
 
                 $this->set('documentFilter', array_key_exists('documentFilter', $this->request['data'])? $this->request['data']['documentFilter'] : 0  );
-                
+
                 if (isset($this->request['data']['documentIds'])) {
         	        $documentIds = $this->request['data']['documentIds'];
                 } else {
                     $documentIds = array();
                 }
-                
+
                 $this->set('documentIds', $documentIds);
-	            $params = explode(',',$this->request['data']['mainValue']);	
+	            $params = explode(',',$this->request['data']['mainValue']);
                 $wordModel = ClassRegistry::init('Word');
                 $wordModel->recursive = 2;
                 $options = array('order'=>'case Word.split when 1 then concat(Word.stem, Word.suffix) else Word.text end','conditions'=> array('Word.id IN ('.QueryBuilder::singleWordChoices($params).')'));
                 if (!empty($documentIds)) {
                     array_push($options['conditions'],'Word.id IN ('.QueryBuilder::singleWordDocuments($documentIds).')');
                 } else {
-                    array_push($options['conditions'],'false');            
+                    array_push($options['conditions'],'false');
                 }
+
+                $totalCount = $wordModel->find('count', $options);
+                $this->set('word_count', $totalCount);
+
+                $page = 0;
+                if (!empty($this->request['data']['page'])) {
+                    $page = $this->request['data']['page'];
+                }
+                $this->set('page', $page);
+
+                $this->set('totalPages', (int) ($totalCount / $this->RESULTS_PER_PAGE) + 1);
+
+                $options['limit'] = $this->RESULTS_PER_PAGE;
+                $options['offset'] = $this->RESULTS_PER_PAGE*$page;
                 $words = $wordModel->find('all', $options);
+
 	            $annotatedWords = array();
 	            $contexts = array();
+
 
                 foreach($words as $word) {
                     array_push($annotatedWords, new AnnotatedWord($word));
                     array_push($contexts, $documentModel->query(" select * from documents inner join languages on languages.id = documents.language_id inner join sentences on documents.id = sentences.document_id and sentences.id = ".$word['Word']['sentence_id']." inner join words on sentences.id = words.sentence_id order by words.position;"));
                 }
-                
+
                 if (count($annotatedWords) != count($contexts)) {
                     die("annotatedWords is of different length than contexts");
                 }
+
                 $this->set('words', $annotatedWords);
                 $this->set('contexts', $contexts);
 
                 $wordAnnotationTypeModel = ClassRegistry::init('WordAnnotationType');
                 $wordAnnotationTypeModel->recursive = 0;
                 $this->set('wordAnnotationTypes', $wordAnnotationTypeModel->find('all'));
-            
+
             } else {
                 $this->Session->setFlash("Empty search query, add search criteria for all searched words.");
-                return $this->redirect(array('action' => 'generator'));    
+                return $this->redirect(array('action' => 'generator'));
             }
-            
+
         }
 
     }
@@ -126,7 +144,7 @@ class StatisticsController extends AppController {
     public function collocations() {
         if ($this->request->is('post')) {
             if (!empty($this->request['data']['mainValue']) &&
-                !empty($this->request['data']['collocationValue'])) {          
+                !empty($this->request['data']['collocationValue'])) {
                 $this->set('mainValue', $this->request['data']['mainValue']);
                 $this->set('collocationValue', $this->request['data']['collocationValue']);
                 $this->set('immediate', $this->request['data']['immediate']);
@@ -146,39 +164,39 @@ class StatisticsController extends AppController {
                 $documentModel->recursive = 1;
                 $documents = $documentModel->find('all');
                 $this->set('documents', $documents);
-                
+
                 if (isset($this->request['data']['documentIds'])) {
         	        $documentIds = $this->request['data']['documentIds'];
                 } else {
                     $documentIds = array();
                 }
-                
+
                 $this->set('documentIds', $documentIds);
 
-	            $mainParams = explode(',',$this->request['data']['mainValue']);	
-	            $collocationParams = explode(',',$this->request['data']['collocationValue']);	
-                
+	            $mainParams = explode(',',$this->request['data']['mainValue']);
+	            $collocationParams = explode(',',$this->request['data']['collocationValue']);
+
                 $sentenceModel = ClassRegistry::init('Sentence');
                 $rawCollocations = $sentenceModel->query(QueryBuilder::collocations($documentIds, $mainParams, $collocationParams));
-                
+
                 $collocations = array();
                 $contexts = array();
 
                 $prevMwId = -1;
-                $minDist  = -1;                    
+                $minDist  = -1;
                 foreach ($rawCollocations as $rawCollocation) {
                     $mwId = $rawCollocation['MW']['id'];
                     $cwId = $rawCollocation['CW']['id'];
                     $dist = $rawCollocation[0]['dist'];
                     if ($mwId != $prevMwId) {
-                        $minDist  = $dist;                    
+                        $minDist  = $dist;
                     }
                     if ($dist == $minDist && $dist < $MAX_DIST + 1) {
                         if ($dist > 0) { // it might be that the word can be its own collocation
 
                             array_push($collocations, array(
                                                           'mwId' => $mwId,
-                                                          'cwId' => $cwId,                                                      
+                                                          'cwId' => $cwId,
                                                           'mwText' => $this->getWordText($rawCollocation['MW']),
                                                           'cwText' => $this->getWordText($rawCollocation['CW']),
                                                           'sepWords' => ($dist-1)
@@ -186,10 +204,10 @@ class StatisticsController extends AppController {
                                       );
                             array_push($contexts, $documentModel->query(" select * from documents inner join languages on languages.id = documents.language_id inner join sentences on documents.id = sentences.document_id and sentences.id = ".$rawCollocation['MW']['sentence_id']." inner join words on sentences.id = words.sentence_id order by words.position;"));
 
-                                    
-                        }            
+
+                        }
                     }
-                    $prevMwId = $mwId;                
+                    $prevMwId = $mwId;
                 }
 
                 if (count($collocations) != count($contexts)) {
@@ -200,12 +218,12 @@ class StatisticsController extends AppController {
                 $this->set('contexts', $contexts);
             } else {
                 $this->Session->setFlash("Empty search query, add search criteria for all searched words.");
-                return $this->redirect(array('action' => 'generator'));    
+                return $this->redirect(array('action' => 'generator'));
             }
         }
 
     }
-    
+
     public function multicollocations() {
         if ($this->request->is('post')) {
             if (!empty($this->request['data']['multiWord1Value']) &&
@@ -225,29 +243,29 @@ class StatisticsController extends AppController {
                 $documentModel->recursive = 1;
                 $documents = $documentModel->find('all');
                 $this->set('documents', $documents);
-                
+
                 if (isset($this->request['data']['documentIds'])) {
         	        $documentIds = $this->request['data']['documentIds'];
                 } else {
                     $documentIds = array();
                 }
-                
-                $this->set('documentIds', $documentIds);
-                
 
-	            $multiWord1Params = explode(',',$this->request['data']['multiWord1Value']);	
-	            $multiWord2Params = explode(',',$this->request['data']['multiWord2Value']);	
-	            $multiWord3Params = explode(',',$this->request['data']['multiWord3Value']);	
-                
+                $this->set('documentIds', $documentIds);
+
+
+	            $multiWord1Params = explode(',',$this->request['data']['multiWord1Value']);
+	            $multiWord2Params = explode(',',$this->request['data']['multiWord2Value']);
+	            $multiWord3Params = explode(',',$this->request['data']['multiWord3Value']);
+
                 $sentenceModel = ClassRegistry::init('Sentence');
                 $rawCollocations = $sentenceModel->query(QueryBuilder::multicollocations($documentIds, $multiWord1Params, $multiWord2Params, $multiWord3Params));
-                
+
                 $contexts = array();
                 foreach ($rawCollocations as $rawCollocation) {
                     array_push($contexts, $documentModel->query("select * from documents inner join languages on languages.id = documents.language_id inner join sentences on documents.id = sentences.document_id and sentences.id = ".$rawCollocation['sentences']['id']." inner join words on sentences.id = words.sentence_id order by words.position;"));
-                
+
                 }
-                
+
                 if (count($rawCollocations) != count($contexts)) {
                     die("annotatedWords is of different length than contexts");
                 }
@@ -256,11 +274,11 @@ class StatisticsController extends AppController {
                 $this->set('contexts', $contexts);
             } else {
                 $this->Session->setFlash("Empty search query, add search criteria for all searched words.");
-                return $this->redirect(array('action' => 'generator'));    
+                return $this->redirect(array('action' => 'generator'));
             }
-        }    
-            
-    }    
+        }
+
+    }
     private function getWordText($wordData) {
         if ($wordData['split']) {
             return $wordData['stem']."|".$wordData['suffix'];
